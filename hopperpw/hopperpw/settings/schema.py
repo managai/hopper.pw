6da2bce83f3ddd695dadabb175e46b128b94bee3
Bruno Santeramo <bruno.santeramo@gmail.com>
2015-10-22 13:36:13 +0200

    settings sections

---
+++ hopperpw/hopperpw/settings/schema.py
@@ -0,0 +1,96 @@
+# coding=utf-8
+
+"""
+Common settings and globals for all environments.
+
+Do not use this setting directly! Instead import these and override DATABASE settings.
+"""
+
+# ######### IMPORT CONFIGURATION
+# ######### END IMPORT CONFIGURATION
+
+
+# ######### PATH CONFIGURATION
+# ######### END PATH CONFIGURATION
+
+
+# ######### DEBUG CONFIGURATION
+# ######### END DEBUG CONFIGURATION
+
+
+# ######### EMAIL CONFIGURATION
+# ######### END EMAIL CONFIGURATION
+
+
+# ######### CACHE CONFIGURATION
+# ######### END CACHE CONFIGURATION
+
+
+# ######### MANAGER CONFIGURATION
+# ######### END MANAGER CONFIGURATION
+
+
+# ######### DATABASE CONFIGURATION
+# ######### END DATABASE CONFIGURATION
+
+
+# ######### GENERAL CONFIGURATION
+# ######### END GENERAL CONFIGURATION
+
+
+# ######### MEDIA CONFIGURATION
+# ######### END MEDIA CONFIGURATION
+
+
+# ######### STATIC FILE CONFIGURATION
+# ######### END STATIC FILE CONFIGURATION
+
+
+# ######### SECRET CONFIGURATION
+# ######### END SECRET CONFIGURATION
+
+
+# ######### SITE CONFIGURATION
+# ######### END SITE CONFIGURATION
+
+
+# ######### FIXTURE CONFIGURATION
+# ######### END FIXTURE CONFIGURATION
+
+
+# ######### TEMPLATE CONFIGURATION
+# ######### END TEMPLATE CONFIGURATION
+
+
+# ######### MIDDLEWARE CONFIGURATION
+# ######### END MIDDLEWARE CONFIGURATION
+
+
+# ######### URL CONFIGURATION
+# ######### END URL CONFIGURATION
+
+
+# ######### APP CONFIGURATION
+# ######### END APP CONFIGURATION
+
+
+# ######### LOGGING CONFIGURATION
+# ######### END LOGGING CONFIGURATION
+
+
+# ######### WSGI CONFIGURATION
+# ######### END WSGI CONFIGURATION
+
+
+# ######### MISCELLANEA CONFIGURATION
+# ######### END MISCELLANEA CONFIGURATION
+
+
+# ######### DJANGO ALLAUTH
+# ######### END DJANGO ALLAUTH
+
+# ######### CELERY CONFIGURATION
+# ######### END CELERY CONFIGURATION
+
+# ######### TOOLBAR CONFIGURATION
+# ######### END TOOLBAR CONFIGURATION
