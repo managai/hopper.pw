775e4d177f012607d8d7cd318a13f40e76c04c78
Bruno Santeramo <bruno.santeramo@gmail.com>
2015-10-25 16:07:04 +0100

    start production configuration

---
+++ hopperpw/hopperpw/settings/hopper1.py
@@ -0,0 +1,109 @@
+# coding=utf-8
+
+"""
+Common settings and globals for all environments.
+
+Do not use this setting directly! Instead import these and override DATABASE settings.
+"""
+
+# ######### IMPORT CONFIGURATION
+# ######### END IMPORT CONFIGURATION
+
+
+# ######### PATH CONFIGURATION
+# ######### END PATH CONFIGURATION
+
+
+# ######### DEBUG CONFIGURATION
+# ######### END DEBUG CONFIGURATION
+
+
+# ######### EMAIL CONFIGURATION
+# ######### END EMAIL CONFIGURATION
+
+
+# ######### CACHE CONFIGURATION
+# ######### END CACHE CONFIGURATION
+
+
+# ######### MANAGER CONFIGURATION
+# ######### END MANAGER CONFIGURATION
+
+
+# ######### DATABASE CONFIGURATION
+# ######### END DATABASE CONFIGURATION
+
+
+# ######### GENERAL CONFIGURATION
+# ######### END GENERAL CONFIGURATION
+
+
+# ######### MEDIA CONFIGURATION
+# ######### END MEDIA CONFIGURATION
+
+
+# ######### STATIC FILE CONFIGURATION
+# ######### END STATIC FILE CONFIGURATION
+
+
+# ######### SECRET CONFIGURATION
+# ######### END SECRET CONFIGURATION
+
+
+# ######### SITE CONFIGURATION
+# Hosts/domain names that are valid for this site
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+BASEDOMAIN = 'www.bc2s.cc'
+
+NONEXISTING_HOST = 'nonexisting.' + BASEDOMAIN
+WWW_HOST = BASEDOMAIN
+WWW_IPV4_HOST = 'ipv4.' + BASEDOMAIN
+WWW_IPV6_HOST = 'ipv6.' + BASEDOMAIN
+WWW_IPV4_IP = '31.14.136.50'
+WWW_IPV6_IP = 'fe80::250:56ff:fe99:5db6'
+
+ALLOWED_HOSTS = [WWW_HOST, WWW_IPV4_HOST, WWW_IPV6_HOST]
+BAD_AGENTS = []
+# ######### END SITE CONFIGURATION
+
+
+# ######### FIXTURE CONFIGURATION
+# ######### END FIXTURE CONFIGURATION
+
+
+# ######### TEMPLATE CONFIGURATION
+# ######### END TEMPLATE CONFIGURATION
+
+
+# ######### MIDDLEWARE CONFIGURATION
+# ######### END MIDDLEWARE CONFIGURATION
+
+
+# ######### URL CONFIGURATION
+# ######### END URL CONFIGURATION
+
+
+# ######### APP CONFIGURATION
+# ######### END APP CONFIGURATION
+
+
+# ######### LOGGING CONFIGURATION
+# ######### END LOGGING CONFIGURATION
+
+
+# ######### WSGI CONFIGURATION
+# ######### END WSGI CONFIGURATION
+
+
+# ######### MISCELLANEA CONFIGURATION
+# ######### END MISCELLANEA CONFIGURATION
+
+
+# ######### DJANGO ALLAUTH
+# ######### END DJANGO ALLAUTH
+
+# ######### CELERY CONFIGURATION
+# ######### END CELERY CONFIGURATION
+
+# ######### TOOLBAR CONFIGURATION
+# ######### END TOOLBAR CONFIGURATION
